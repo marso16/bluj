1f810f84d2139414d700a214cb647bec0b364dfc
marso16 <marcelino.keyrou16@gmail.com>
2026-09-01 17:38:45 +0300

    fix: add basePath /studio to Sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,6 +7,7 @@ import { structure } from './src/sanity/structure'
 export default defineConfig({
   name: 'bluj',
   title: 'BluJ',
+  basePath: '/studio',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   plugins: [structureTool({ structure }), visionTool()],
