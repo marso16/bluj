90c5e5eea085f8a202bfef63d50ea614f87e3b7a
marso16 <marcelino.keyrou16@gmail.com>
2026-09-02 21:49:57 +0300

    :)

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'sanity'
-import { structureTool } from 'sanity/structure'
-import { visionTool } from '@sanity/vision'
-import { schemaTypes } from './src/sanity/schemaTypes'
-import { structure } from './src/sanity/structure'
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./src/sanity/schemaTypes";
+import { structure } from "./src/sanity/structure";
 
 export default defineConfig({
-  name: 'bluj',
-  title: 'BluJ',
-  basePath: '/studio',
+  name: "bluj",
+  title: "BluJ",
+  basePath: "/studio",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   plugins: [structureTool({ structure }), visionTool()],
   schema: { types: schemaTypes },
-})
+});
